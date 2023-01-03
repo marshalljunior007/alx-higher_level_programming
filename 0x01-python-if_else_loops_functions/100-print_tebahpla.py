86a87b166e3238346f2b0696d622326602e5e539
marshalljunior007 <marshalljunior007@gmail.com>
2023-01-03 04:48:10 -0800

    hello world

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,3 @@
-n range(ord('z'), ord('a') - 1, -2):
+#!/usr/bin/python3
+for b in range(ord('z'), ord('a') - 1, -2):
     print("{:c}{:s}".format(b, chr(b - 33)), end="")
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,5 @@
- in range(0, 9):
+#!/usr/bin/python3
+for i in range(0, 9):
     for j in range(i + 1, 10):
         if i == 8:
             print("{}{}".format(i, j))
